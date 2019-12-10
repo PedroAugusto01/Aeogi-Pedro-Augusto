1ea9bcd2fc71f8578f28a3f70d34934040bce517
PedroAugusto01 <58372059+PedroAugusto01@users.noreply.github.com>
2019-12-10 16:45:33 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<center>
+<h1>Pesquisar Pokemon</h1>
+<form method="POST" action="pesquisar.php">
+	Pesquisar:<input type="text" name="pesquisar" placeholder="PESQUISAR">
+	<input type="submit" value="ENVIAR">
+	<h1>Caso tenha dúvidas, clique no botão a baixo para efetuar um download de uma planilha com os códigos e os nomes dos pokemons.</h1>
+	<button type="submit" formaction="https://gofile.io/?c=a6lnSP">Download</button>
+	</center>
+</form>
