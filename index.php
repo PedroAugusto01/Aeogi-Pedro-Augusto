e83c87901ef4f9d1c164e965fda9921e87f532b5
PedroAugusto01 <58372059+PedroAugusto01@users.noreply.github.com>
2019-12-10 22:15:03 -0300

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	Pesquisar: <input type="text" name="pesquisar" placeholder="PESQUISAR">
 	<input type="submit" value="ENVIAR">
 	<p></p>
-	<div style="border: 3px solid black">Caso tenha dúvidas, clique no botão a baixo para efetuar um download de uma planilha com os códigos e os nomes dos pokémons.</div>
+	<div style="border: 3px solid black">Caso tenha dúvidas, clique no botão a baixo para efetuar um download de uma planilha com os códigos e os nomes dos pokémons. Caso for usar código por favor, inserir algarismos de três digitos, exemplo: 001, 024.</div>
 	<p></p>
 	<button type="submit" formaction="https://gofile.io/?c=a6lnSP">Download</button>
 	</center>
