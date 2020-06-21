7b53db98f46adef2cf27f99ad98ccfa2e731c358
Pedro Augusto <pedro.ams26@gmail.com>
2020-06-21 00:28:25 -0300

    Change code for plus simplific and change layout

--- index.php
+++ index.php
@@ -1,12 +1,48 @@
 <center>
-<div style="border: 3px solid black"><h1>Pesquisar Pokémon</h1></div>
-<p></p>
-<form method="POST" action="pesquisar.php">
-	Pesquisar: <input type="text" name="pesquisar" placeholder="PESQUISAR">
-	<input type="submit" value="ENVIAR">
+	<div style="border: 3px solid black">
+		<h1>Pesquisar Pokémon</h1>
+	</div>
 	<p></p>
-	<div style="border: 3px solid black">Caso tenha dúvidas, clique no botão a baixo para efetuar um download de uma planilha com os códigos e os nomes dos pokémons. Caso for usar código por favor, inserir algarismos de três digitos, exemplo: 001, 024.</div>
+
+	<head>
+		<style>
+			table,
+			th,
+			td {
+				border: 1px solid black;
+			}
+		</style>
+	</head>
+	<table>
+		<th>
+			<form method="POST" action="pesquisar.php">
+		<th>
+			Pesquisar: <input type="text" name="pesquisar" placeholder="PESQUISAR">
+			<input type="submit" value="ENVIAR">
+		</th>
+		</th>
+	</table>
 	<p></p>
-	<button type="submit" formaction="https://gofile.io/?c=a6lnSP">Download</button>
-	</center>
+
+	<head>
+		<style>
+			table,
+			th,
+			td {
+				border: 1px solid black;
+			}
+		</style>
+	</head>
+
+	<body>
+		<table style="width:100%">
+			<th>Caso tenha dúvidas, clique no botão ao lado para efetuar um download de uma planilha com os códigos e os nomes dos pokémons.</th>
+
+			<th>
+				<button type="submit" formaction="http://download1494.mediafire.com/reoh39lpdq6g/0p59rpqkkiayjii/Pokemon+Import.csv">Download
+			</th>
+			</button>
+		</table>
+
+</center>
 </form>
